ea379b6fb4f8254d06e768a42816545b09df9a3d
VCShen <37541252+noworktoday00@users.noreply.github.com>
2022-03-09 18:50:22 +0800

    Vr-5

--- JS/recommend.js
+++ JS/recommend.js
@@ -1,22 +1,41 @@
 // 按鈕Dom
 let sendButton = document.querySelector('#sendbutton');
-
+const url="https://script.google.com/macros/s/AKfycbxZLEu3r3WrUGcvb5Ajc05AwUDpNiXKAkRrNo4ldE_1i9v34ktQoUe2DBqRuvM62s12/exec"
 
 function send() { 
-    const userName = document.querySelector('#userName').Value;
-    const gender = document.querySelector('#gender').Value;
-    const age = document.querySelector('#age').Value;
-    const phone = document.querySelector('#phone').Value;
-    const mail = document.querySelector('#mail').Value;
-    const school = document.querySelector('#school').Value;
-    const program = document.querySelector('#program').Value;
-    const college = document.querySelector('#college').Value;
-    const degree = document.querySelector('#degree').Value;
-    const grade = document.querySelector('#grade').Value;
-    const techtype = document.querySelector('#techtype').Value;
-    const readingtype = document.querySelector('#readingtype').Value;
-    const recommendbook = document.querySelector('#recommendbook').Value;
-    console.log('送出按鈕');
+    const userName = document.getElementById('userName').value;
+    const userGender = document.getElementById('userGender').value;
+    const userAge = document.getElementById('userAge').value;
+    const userPhone = document.getElementById('userPhone').value;
+    const userMail = document.getElementById('userMail').value;
+    const userSchool = document.getElementById('userSchool').value;
+    const userProgram = document.getElementById('userProgram').value;
+    const userCollege = document.getElementById('userCollege').value;
+    const userDegree = document.getElementById('userDegree').value;
+    const userGrade = document.getElementById('userGrade').value;
+    const techtype = document.getElementById('techtype').value;
+    const readingtype = document.getElementById('readingtype').value;
+    const recommendbook = document.getElementById('recommendbook').value;
+
+    axios.post(url,
+        {data: {
+            "name": userName,
+            "gender": userGender,
+            "age": userAge,
+            "phone":userPhone,
+            "mail": userMail,
+            "school":userSchool,
+            "program":userProgram,
+            "college":userCollege,
+            "degree":userDegree,
+            "grade":userGrade,
+            "techtype":techtype,
+            "readingtype":readingtype,
+            "recommendbook":recommendbook,
+        }})
+        .then(function(res){
+            alert(`推薦成功!!!`)
+        });
 };
 
 sendButton.addEventListener('click',send);
